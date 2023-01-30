2354477d3b4697d0178a36aad1ead75a4642fc7c
Zhan <221122@astanait.edu.kz>
2023-01-30 19:15:34 +0600

    nothing special, just adding classes to package and sharing responsibilities

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        DBMethods test = new DBMethods();
-        test.connect_to_DB("DatabaseOne", "postgres", "0311");
-
-    }
-}
