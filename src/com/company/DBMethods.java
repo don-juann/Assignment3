789bb7cdf0ae5e2560079443983aaf94f1646493
Zhan <221122@astanait.edu.kz>
2023-02-01 21:52:59 +0600

    All basic methods to manipulate data in PostgreSQL

--- src/com/company/DBMethods.java
+++ src/com/company/DBMethods.java
@@ -1,16 +1,19 @@
 package com.company;
 
+import jdk.jshell.spi.ExecutionControlProvider;
+
+import javax.swing.plaf.nimbus.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DBMethods {
     public Connection connect_to_DB(String DBName, String Username, String password ){
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection
-                    ("jdbc:postgresql://localhost:5432/"+DBName,
-                            Username, password);
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+DBName, Username, password);
             if(connection!=null){
                 System.out.println("Connection established");
             }else{
@@ -20,5 +23,128 @@ public class DBMethods {
             System.out.println(e);
         }
         return connection;
+    }
+    public void createTable(Connection connection, String table_name){
+        Statement stmt;
+        try{
+            String query = "create table " + table_name + "(empid SERIAL, name varchar(200), address varchar(200), primary key(empid));";
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Table Created");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void insert_row(Connection connection, String table_name, String name, String address){
+        Statement stmt;
+        try{
+            String query = String.format("insert into %s(name, address) values('%s','%s');", table_name, name, address);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Row Inserted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_row_by_name(Connection connection, String table_name, String name){
+        Statement stmt;
+        try{
+            String query = String.format("delete from %s where name = '%s'", table_name, name);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Data Deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_row_by_id(Connection connection, String table_name, int id){
+        Statement stmt;
+        try{
+            String query = String.format("delete from %s where empid = '%s'", table_name, id);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Data Deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public void read_data(Connection connection, String table_name){
+        Statement stmt;
+        ResultSet rs = null;
+        try{
+            String query = String.format("select * from %s", table_name);
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid") + " ");
+                System.out.print(rs.getString("name")+ " ");
+                System.out.println(rs.getString("address")+ " ");
+            }
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void update_name(Connection connection, String table_name, String new_name, String old_name){
+        Statement stmt;
+        try{
+            String query = String.format("update %s set name='%s' where name = '%s'", table_name, new_name, old_name);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Data Updated");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void search_by_name(Connection connection, String table_name, String name){
+        Statement stmt;
+        ResultSet rs = null;
+        try{
+            String query = String.format("select * from %s where name = '%s'",table_name, name);
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.println(rs.getString("address") + " ");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void search_by_id(Connection connection, String table_name, int id){
+        Statement stmt;
+        ResultSet rs = null;
+        try{
+            String query = String.format("select * from %s where empid = '%s'",table_name, id);
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.println(rs.getString("address") + " ");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_table(Connection connection, String table_name){
+        Statement stmt;
+        try{
+            String query = String.format("drop table %s", table_name);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Table Deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
