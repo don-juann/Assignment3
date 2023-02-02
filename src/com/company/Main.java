5ddb2fc2f84fcc899a9cfc0a3f6696e54abb15c4
kbbr <k.akdaulet@gmail.com>
2023-02-02 13:22:32 +0600

    I have created subclass of DBMethods. Next time it will be serve like superclass for Employeea and Client

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,15 @@
 package com.company;
 
-import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
-        DBMethods database = new DBMethods();
-        Connection connection = database.connect_to_DB("DatabaseOne", "postgres", "0311");
+        DBMethods test = new DBMethods();
+        test.connect_to_DB("DatabaseOne", "postgres", "0311");
 
-        //database.createTable(connection, "employee");
-        //database.insert_row(connection, "employee", "Zhan", "Astana");
-        //database.update_name(connection, "employee", "Daulet", "Zhan" );
-        //database.read_data(connection, "employee");
-        //database.search_by_name(connection, "employee", "Zhan");
-        //database.search_by_id(connection, "employee", 1);
-        //database.delete_row_by_id(connection, "employee", 2);
-        //database.read_data(connection, "employee");
-        //database.delete_table(connection, "employee");
+        /*
+        database connection, signing in, signing up as customer - Zhan
+        signing in, signing up as administrator - Akdaulet
+        checking balance and user information - Tolegen
+        making deposit and withdrawing money - Aitbek
+         */
     }
 }
